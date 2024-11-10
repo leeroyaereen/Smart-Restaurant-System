a9b77d043d57e0fea68ad916491346180383e11b
leeroyarin <142387082+leeroyaereen@users.noreply.github.com>
2024-11-10 22:12:57 +0545

    Added Required Pages for Adding And Creating Database and its elements

--- src/PHP/DatabaseUtilities.php
+++ src/PHP/DatabaseUtilities.php
@@ -9,28 +9,44 @@
 
         private static $restuarantDatabaseConnection;
 
-        public static function connectToMainDatabase(){
-            self::$mainConnection = mysqli_connect(self::$mainServername,self::$mainUsername, self::$mainPassword,self:: $mainDatabase);
-            if(!self::$mainConnection){
-                echo "<script>alert('Error Connection')</script>";
+        public static function connectToMainDatabase() {
+            self::$mainConnection = mysqli_connect(self::$mainServername, self::$mainUsername, self::$mainPassword, self::$mainDatabase);
+            
+            if (!self::$mainConnection) {
+                echo "<script>alert('Error Connecting to Database')</script>";
                 return false;
             }
             return true;
         }
+    
+        public static function CreateDatabase($restaurantName) {
+            //Just for debugging cases
+            // echo "<script>alert('Connecting to Database')</script>";
+            
+            // Ensure to call the function correctly
+            $result = self::connectToMainDatabase();
+    
+            if ($result) {
+                //sql statement to search if there is any database named same as the parametered name
+                $checkQuery = "SELECT SCHEMA_NAME FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = '$restaurantName'";
+                $checkDB_Result = mysqli_query(self::$mainConnection,$checkQuery);
 
-        public static function CreateDatabase($restaurantName){
-            $result = connectToMainDatabase();
-            if($result){
-                $sql = "CREATE DATABASE '$restaurantName'";
-                $res = self::$mainConnection->query($sql);
-                if($res === true){
-                    echo "<script>alert('Database $restaurantName Created Successfully ')</script>";
-                }else{
-                    echo "<script>alert('Error Creating Database')</script>";
+                //if there exists any other database with the same name the count rises to 1 and the creation of database is restricted
+                if(mysqli_num_rows($checkDB_Result)>0){
+                    echo "<script>alert('Database you tried to create already exists')</script>";
+                    return;
+                }
+                // Correct the SQL syntax
+                $sql = "CREATE DATABASE `$restaurantName`";
+                $res = mysqli_query(self::$mainConnection, $sql); // Using mysqli_query with the connection resource
+    
+                if ($res === true) {
+                    echo "<script>alert('Database $restaurantName Created Successfully')</script>";
+                } else {
+                    echo "<script>alert('Error Creating Database: '" . mysqli_error(self::$mainConnection).")</script>";
                 }
             }
         }
-
         public static function CreateTables($databaseName){
             self::$restuarantDatabaseConnection = mysqli_connect(self::$mainServername,self::$mainUsername, self::$mainPassword,$databaseName);
             if(!self::$restuarantDatabaseConnection){
@@ -64,33 +80,37 @@
 
             $sqlFoodCategoryTable = "CREATE TABLE FoodCategory (
                 Category_ID INT PRIMARY KEY AUTO_INCREMENT,
-                FoodItem_ID INT,
-                CategoryName VARCHAR(100),
-                FOREIGN KEY (FoodItem_ID) REFERENCES FoodItems(FoodItem_ID) 
+                CategoryName VARCHAR(100) NOT NULL
             );";
 
             $sqlFoodItem = "CREATE TABLE FoodItems (
                 FoodItem_ID INT PRIMARY KEY AUTO_INCREMENT,
                 FoodName VARCHAR(100) NOT NULL,
-                FoodRating DECIMAL(1, 1),
-                FoodPreparationTime INT, -- in min
+                FoodType VARCHAR(30),
+                Category_ID INT NOT NULL,
+                FoodRating DECIMAL(2, 1),
+                FoodPreparationTime INT,
                 FoodReview TEXT,
                 FoodDescription TEXT,
-                FoodImage VARCHAR(255), -- as link
+                FoodImage VARCHAR(255),
                 FoodPrice DECIMAL(10, 2),
                 FoodAvailability BOOLEAN DEFAULT TRUE,
-                TotalOrders INT DEFAULT 0
+                TotalOrders INT DEFAULT 0,
+                FOREIGN KEY (Category_ID) REFERENCES FoodCategory(Category_ID)
             );";
 
-            $sql = $sqlFoodItem+$sqlFoodCategoryTable+$sqlMenuTable+$sqlRoleTable+$sqlUserTable;
 
-            $res = self::$restuarantDatabaseConnection->query($sql);
-            if($res){
-                echo "<script>alert('Created Table Successfully')</script>";
-            }else{
-                echo "<script>alert('Error Connection $res')</script>";
+            //add all the sql commands in array and executing one by one
+            $queries = [$sqlFoodCategoryTable,$sqlFoodItem,$sqlMenuTable,$sqlRoleTable,$sqlUserTable];
+            foreach($queries as $sql){
+                $res = self::$restuarantDatabaseConnection->query($sql);
+                if(!$res){
+                    echo "<script>alert('Error Inserting $res')</script>";
+                }
+            }                    
+            echo "<script>alert('Created Table Successfully')</script>";
 
-            }
+            
         }
         // Creates food item in the menu
         public static function CreateItems($foodItem){
@@ -102,8 +122,8 @@
                 }
                 
 
-                $sql = "INSERT INTO FoodItems (FoodName, FoodRating, FoodPreparationTime, FoodReview, FoodDescription, FoodImage, FoodPrice, FoodAvailability, TotalOrders) 
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                $sql = "INSERT INTO FoodItems (FoodName, FoodType, Category_ID, FoodRating, FoodPreparationTime, FoodReview, FoodDescription, FoodImage, FoodPrice, FoodAvailability, TotalOrders) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
                 //compiles the structure without defining parameters without executing it.
                 $statement = self::$restuarantDatabaseConnection->prepare($sql);
 
@@ -111,14 +131,15 @@
                 if($statement===FALSE){
                     echo "<script>alert('There was error preparing statement')</script>";
                 }
-
                 //set variables of the sql
                 //s represents string type
                 //d represents double type
                 //i represents integer type
                 //b represents blob (objects like image and files that are heavy in size)
-                $statement->bind_param('sdisssdii',
+                $statement->bind_param('ssidisssdii',
                     $foodItem->FoodName,
+                    $foodItem->FoodType,
+                    $foodItem->FoodCategory,
                     $foodItem->FoodRating,
                     $foodItem->FoodPreparationTime,
                     $foodItem->FoodReview,
@@ -134,43 +155,11 @@
                 //check if the statement execution was successfull or not
                 if($res){
                     echo "<script>alert('Food Item Added Successfully')</script>";
+                    return true; 
                 }
                 else{
                     $statement->close();
                     echo "<script>alert('There was error executing statement')</script>";
-                    return false;
-                }
-
-
-                //Get the ID of the newly inserted food item
-                $newFoodItemId = $statement->insert_id;
-                $statement->close();
-
-                // Step 3: Link the food item to the category in the FoodCategory table
-                $statementCategory = self::$restuarantDatabaseConnection->prepare(
-                    "INSERT INTO FoodCategory (FoodItem_ID, CategoryName) 
-                    VALUES ( ?, (SELECT CategoryName FROM FoodCategory WHERE Category_ID = ?))"
-                );
-
-                if (!$statementCategory) {
-                    echo "<script>alert('Error preparing SQL statement for FoodCategory.')</script>";
-                    return false;
-                }
-
-                // Bind the parameters
-                $statementCategory->bind_param("is", 
-                    $newFoodItemId, 
-                    $foodItem->FoodCategory
-                );
-
-                // Execute the insertion into FoodCategory
-                if ($statementCategory->execute()) {
-                    echo "<script>alert('Food item and category link created successfully!')</script>";
-                    $statementCategory->close();
-                    return true;
-                } else {
-                    echo "<script>alert('Error linking food item to category: " . $$statementCategory->error . "')</script>";
-                    $statementCategory->close();
                     return false;
                 }
             }else{
@@ -178,11 +167,52 @@
                 return false;
             }
         }
+
+
+        public static function CreateCategory($categoryName){
+            if (!self::$restuarantDatabaseConnection) {
+                echo "<script>alert('Not connected to the restaurant database.')</script>";
+                return false;
+            }
+            
+            $sql = "INSERT INTO foodcategory (CategoryName) VALUES ('$categoryName')";
+            $res = mysqli_query(self::$restuarantDatabaseConnection,$sql);
+            if($res){
+                echo "<script>alert('The Category has been added')</script>";
+            }else{
+                echo "<script>alert('There was error adding category')</script>";
+
+            }
+        }
+        //##remaining Functions
+        //change password
+        //change username
+        //change restaurantDatabaseConnection.
+        public static function ChangeRestaurant($databaseName){
+            self::$restuarantDatabaseConnection = mysqli_connect(self::$mainServername,self::$mainUsername, self::$mainPassword,$databaseName);
+
+        }
+
+        public static function GetRestaurantConnection(){
+            return self::$restuarantDatabaseConnection;
+        }
+        public static function GetRestaurantConnectionWithName($databaseName){
+            self::$restuarantDatabaseConnection = mysqli_connect(self::$mainServername,self::$mainUsername, self::$mainPassword,$databaseName);
+            if(self::$restuarantDatabaseConnection){
+                return self::$restuarantDatabaseConnection;
+            }        
+        }
+
+        public static function RunCommandOnRestaurantDatabase(){
+
+        }
     }
 
     Class FoodItem{
         public $FoodItem_ID = 00000;
         public $FoodName = "Unnamed";
+        public $FoodType = "";
+        public $FoodCategory;
         public $FoodRating = 0.0;
         public $FoodPreparationTime = 30;
         public $FoodReview = "No Food Review";
@@ -191,6 +221,5 @@
         public $FoodPrice = 0;
         public $FoodAvailability = false;
         public $TotalOrders = 0;
-        public $FoodCategory = 'None';
     }
 ?>
