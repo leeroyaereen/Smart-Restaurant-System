d388c7f825c3acab3fda0c86c4b0be0f8c19fb64
shovit <shovitbhatta4@gmail.com>
2024-11-28 11:30:04 +0545

    Add menu items dynamically with js

--- public/assets/js/ContentMenu.js
+++ public/assets/js/ContentMenu.js
@@ -1,8 +1,28 @@
+const menuItemList = document.querySelector("#MenuItemList");
+const menuItemTemplate = document.querySelector("#MenuItemTemplate");
+
 window.onload = loadMenu;
+
+document.querySelector("#OrderButton Button").onclick = () => {
+	window.location.href = "confirm-order";
+};
+
 function loadMenu() {
-    fetch(BASE_PATH + '/api/getFoodItems')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    });
+	fetch(BASE_PATH + "/api/getFoodItems")
+		.then((response) => response.json())
+		.then((data) => {
+			fillMenu(data);
+		});
+}
+
+function fillMenu(data) {
+	data.forEach((item) => {
+		let templateClone = menuItemTemplate.content.cloneNode(true);
+		let menuItem = templateClone.querySelector("#MenuItem");
+		menuItem.querySelector("#MenuItemName").innerText = item.FoodName;
+		menuItem.querySelector("#MenuItemSubCategory").innerText = item.FoodType;
+		menuItem.querySelector("#MenuItemPrice span").innerText = item.FoodPrice;
+		menuItem.querySelector("#MenuItemDuration span").innerText = item.FoodPreparationTime+" min";
+		menuItemList.appendChild(menuItem);
+	});
 }
--- public/views/ContentMenu.php
+++ public/views/ContentMenu.php
@@ -14,7 +14,9 @@
 	<body>
 		<div id="MenuHeader" class="section Header">
 			<div id="MenuHeaderTitle" class="HeaderTitle">Menu</div>
-			<div id="MenuHeaderTable" class="HeaderSubTitle">Customer Name: Ram Nepali</div>
+			<div id="MenuHeaderTable" class="HeaderSubTitle">
+				Customer Name: Ram Nepali
+			</div>
 			<div id="searchBoxContainer">
 				<form action="">
 					<svg
@@ -46,331 +48,121 @@
 			</div>
 		</div>
 		<div id="MenuItemList" class="section Body">
-			<div id='MenuItem'>
-				<div id='MenuItemImage'>
-					<img
-						src='https://t4.ftcdn.net/jpg/06/49/25/27/360_F_649252736_LK6ign50vHZicrNR6Fe2mSpmPDNupx6Y.jpg'
-						alt=''
-						width='95'
-						height='95'
-						class='ItemImage'
-					/>
-					<div>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							width='24'
-							height='24'
-							viewBox='0 0 24 24'
-							fill='none'
-							stroke='#FE724C'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
-							class='icon icon-tabler icons-tabler-outline icon-tabler-info-circle'
-						>
-							<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-							<path d='M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0' />
-							<path d='M12 9h.01' />
-							<path d='M11 12h1v4h1' />
-						</svg>
-						<span>More info</span>
+			<template class="hidden-template" id="MenuItemTemplate">
+				<div id="MenuItem">
+					<div id="MenuItemImage">
+						<img
+							src="https://t4.ftcdn.net/jpg/06/49/25/27/360_F_649252736_LK6ign50vHZicrNR6Fe2mSpmPDNupx6Y.jpg"
+							alt=""
+							width="95"
+							height="95"
+							class="ItemImage"
+						/>
+						<div>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="#FE724C"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								class="icon icon-tabler icons-tabler-outline icon-tabler-info-circle"
+							>
+								<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+								<path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+								<path d="M12 9h.01" />
+								<path d="M11 12h1v4h1" />
+							</svg>
+							<span>More info</span>
+						</div>
 					</div>
-				</div>
-				<div id='MenuItemDetails'>
-						<div id='MenuItemTitle'>
-							<div id='MenuItemName'>Chicken Momo</div>
-							<div id='MenuItemRating'>
+					<div id="MenuItemDetails">
+						<div id="MenuItemTitle">
+							<div id="MenuItemName">Chicken Momo</div>
+							<div id="MenuItemRating">
 								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									width='16'
-									height='16'
-									viewBox='0 0 24 24'
-									fill='#FFE607'
-									class='icon icon-tabler icons-tabler-filled icon-tabler-star'
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									viewBox="0 0 24 24"
+									fill="#FFE607"
+									class="icon icon-tabler icons-tabler-filled icon-tabler-star"
 								>
-									<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 									<path
-										d='M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z'
+										d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
 									/>
 								</svg>
 								<span>3.4</span>
 							</div>
 						</div>
-						<div id='MenuItemSubCategory'>Fried</div>
-						<div id='MenuItemPrice' class='ItemPrice'>Rs 220</div>
-							<div id='MenuItemFooter' class='ItemFooter'>
-								<div id='MenuItemDuration'>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										width='24'
-										height='24'
-										viewBox='0 0 24 24'
-										fill='none'
-										stroke='currentColor'
-										stroke-width='2'
-										stroke-linecap='round'
-										stroke-linejoin='round'
-										class='icon icon-tabler icons-tabler-outline icon-tabler-clock'
-									>
-										<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-										<path d='M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0' />
-										<path d='M12 7v5l3 3' />
-									</svg>
-									<span>10 mins </span>
-								</div>
-								<div id='MenuItemQuantity' class='ItemQuantity'>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										width='45'
-										height='45'
-										viewBox='0 0 24 24'
-										fill='none'
-										stroke='#FE724C'
-										stroke-width='1'
-										stroke-linecap='round'
-										stroke-linejoin='round'
-										class='icon icon-tabler icons-tabler-outline icon-tabler-circle-minus'
-									>
-										<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-										<path d='M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0' />
-										<path d='M9 12l6 0' />
-									</svg>
-									<span>01</span>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										width='45'
-										height='45'
-										viewBox='0 0 24 24'
-										fill='#FE724C'
-										class='icon icon-tabler icons-tabler-filled icon-tabler-circle-plus'
-									>
-										<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-										<path
-											d='M4.929 4.929a10 10 0 1 1 14.141 14.141a10 10 0 0 1 -14.14 -14.14zm8.071 4.071a1 1 0 1 0 -2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 1 0 2 0v-2h2a1 1 0 1 0 0 -2h-2v-2z'
-										/>
-									</svg>
-								</div>
-							</div>
-				</div>
-			</div>	
-			<div id='MenuItem'>
-				<div id='MenuItemImage'>
-					<img
-						src='https://t4.ftcdn.net/jpg/06/49/25/27/360_F_649252736_LK6ign50vHZicrNR6Fe2mSpmPDNupx6Y.jpg'
-						alt=''
-						width='95'
-						height='95'
-						class='ItemImage'
-					/>
-					<div>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							width='24'
-							height='24'
-							viewBox='0 0 24 24'
-							fill='none'
-							stroke='#FE724C'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
-							class='icon icon-tabler icons-tabler-outline icon-tabler-info-circle'
-						>
-							<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-							<path d='M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0' />
-							<path d='M12 9h.01' />
-							<path d='M11 12h1v4h1' />
-						</svg>
-						<span>More info</span>
-					</div>
-				</div>
-				<div id='MenuItemDetails'>
-						<div id='MenuItemTitle'>
-							<div id='MenuItemName'>Chicken Momo</div>
-							<div id='MenuItemRating'>
+						<div id="MenuItemSubCategory">Fried</div>
+						<div id="MenuItemPrice" class="ItemPrice">Rs <span>220</span></div>
+						<div id="MenuItemFooter" class="ItemFooter">
+							<div id="MenuItemDuration">
 								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									width='16'
-									height='16'
-									viewBox='0 0 24 24'
-									fill='#FFE607'
-									class='icon icon-tabler icons-tabler-filled icon-tabler-star'
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									class="icon icon-tabler icons-tabler-outline icon-tabler-clock"
 								>
-									<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+									<path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+									<path d="M12 7v5l3 3" />
+								</svg>
+								<span>10 mins</span>
+							</div>
+							<div id="MenuItemQuantity" class="ItemQuantity">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="45"
+									height="45"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="#FE724C"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									class="icon icon-tabler icons-tabler-outline icon-tabler-circle-minus"
+								>
+									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+									<path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+									<path d="M9 12l6 0" />
+								</svg>
+								<span>01</span>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="45"
+									height="45"
+									viewBox="0 0 24 24"
+									fill="#FE724C"
+									class="icon icon-tabler icons-tabler-filled icon-tabler-circle-plus"
+								>
+									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 									<path
-										d='M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z'
+										d="M4.929 4.929a10 10 0 1 1 14.141 14.141a10 10 0 0 1 -14.14 -14.14zm8.071 4.071a1 1 0 1 0 -2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 1 0 2 0v-2h2a1 1 0 1 0 0 -2h-2v-2z"
 									/>
 								</svg>
-								<span>3.4</span>
 							</div>
 						</div>
-						<div id='MenuItemSubCategory'>Fried</div>
-						<div id='MenuItemPrice' class='ItemPrice'>Rs 220</div>
-							<div id='MenuItemFooter' class='ItemFooter'>
-								<div id='MenuItemDuration'>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										width='24'
-										height='24'
-										viewBox='0 0 24 24'
-										fill='none'
-										stroke='currentColor'
-										stroke-width='2'
-										stroke-linecap='round'
-										stroke-linejoin='round'
-										class='icon icon-tabler icons-tabler-outline icon-tabler-clock'
-									>
-										<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-										<path d='M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0' />
-										<path d='M12 7v5l3 3' />
-									</svg>
-									<span>10 mins </span>
-								</div>
-								<div id='MenuItemQuantity' class='ItemQuantity'>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										width='45'
-										height='45'
-										viewBox='0 0 24 24'
-										fill='none'
-										stroke='#FE724C'
-										stroke-width='1'
-										stroke-linecap='round'
-										stroke-linejoin='round'
-										class='icon icon-tabler icons-tabler-outline icon-tabler-circle-minus'
-									>
-										<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-										<path d='M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0' />
-										<path d='M9 12l6 0' />
-									</svg>
-									<span>01</span>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										width='45'
-										height='45'
-										viewBox='0 0 24 24'
-										fill='#FE724C'
-										class='icon icon-tabler icons-tabler-filled icon-tabler-circle-plus'
-									>
-										<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-										<path
-											d='M4.929 4.929a10 10 0 1 1 14.141 14.141a10 10 0 0 1 -14.14 -14.14zm8.071 4.071a1 1 0 1 0 -2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 1 0 2 0v-2h2a1 1 0 1 0 0 -2h-2v-2z'
-										/>
-									</svg>
-								</div>
-							</div>
-				</div>
-			</div>	
-			<div id='MenuItem'>
-				<div id='MenuItemImage'>
-					<img
-						src='https://t4.ftcdn.net/jpg/06/49/25/27/360_F_649252736_LK6ign50vHZicrNR6Fe2mSpmPDNupx6Y.jpg'
-						alt=''
-						width='95'
-						height='95'
-						class='ItemImage'
-					/>
-					<div>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							width='24'
-							height='24'
-							viewBox='0 0 24 24'
-							fill='none'
-							stroke='#FE724C'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
-							class='icon icon-tabler icons-tabler-outline icon-tabler-info-circle'
-						>
-							<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-							<path d='M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0' />
-							<path d='M12 9h.01' />
-							<path d='M11 12h1v4h1' />
-						</svg>
-						<span>More info</span>
 					</div>
 				</div>
-				<div id='MenuItemDetails'>
-						<div id='MenuItemTitle'>
-							<div id='MenuItemName'>Chicken Momo</div>
-							<div id='MenuItemRating'>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									width='16'
-									height='16'
-									viewBox='0 0 24 24'
-									fill='#FFE607'
-									class='icon icon-tabler icons-tabler-filled icon-tabler-star'
-								>
-									<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-									<path
-										d='M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z'
-									/>
-								</svg>
-								<span>3.4</span>
-							</div>
-						</div>
-						<div id='MenuItemSubCategory'>Fried</div>
-						<div id='MenuItemPrice' class='ItemPrice'>Rs 220</div>
-							<div id='MenuItemFooter' class='ItemFooter'>
-								<div id='MenuItemDuration'>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										width='24'
-										height='24'
-										viewBox='0 0 24 24'
-										fill='none'
-										stroke='currentColor'
-										stroke-width='2'
-										stroke-linecap='round'
-										stroke-linejoin='round'
-										class='icon icon-tabler icons-tabler-outline icon-tabler-clock'
-									>
-										<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-										<path d='M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0' />
-										<path d='M12 7v5l3 3' />
-									</svg>
-									<span>10 mins </span>
-								</div>
-								<div id='MenuItemQuantity' class='ItemQuantity'>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										width='45'
-										height='45'
-										viewBox='0 0 24 24'
-										fill='none'
-										stroke='#FE724C'
-										stroke-width='1'
-										stroke-linecap='round'
-										stroke-linejoin='round'
-										class='icon icon-tabler icons-tabler-outline icon-tabler-circle-minus'
-									>
-										<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-										<path d='M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0' />
-										<path d='M9 12l6 0' />
-									</svg>
-									<span>01</span>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										width='45'
-										height='45'
-										viewBox='0 0 24 24'
-										fill='#FE724C'
-										class='icon icon-tabler icons-tabler-filled icon-tabler-circle-plus'
-									>
-										<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-										<path
-											d='M4.929 4.929a10 10 0 1 1 14.141 14.141a10 10 0 0 1 -14.14 -14.14zm8.071 4.071a1 1 0 1 0 -2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 1 0 2 0v-2h2a1 1 0 1 0 0 -2h-2v-2z'
-										/>
-									</svg>
-								</div>
-							</div>
-				</div>
-			</div>	
+			</template>
 		</div>
 		<div id="OrderOverview" class="section Footer">
 			<div id="OrderDetails">
-				<div id="OrderCostBlock">Total: <span id="OrderCost">Rs 10132</span></div>
+				<div id="OrderCostBlock">
+					Total: <span id="OrderCost">Rs 10132</span>
+				</div>
 				<div id="OrderTimeBlock">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -384,7 +176,6 @@
 						stroke-linejoin="round"
 						class="icon icon-tabler icons-tabler-outline icon-tabler-clock"
 					>
-					
 						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 						<path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
 						<path d="M12 7v5l3 3" />
@@ -393,7 +184,7 @@
 				</div>
 			</div>
 			<div id="OrderButton" class="DefaultOrderButton">
-				<button onclick="window.location.href='confirm-order'">
+				<button>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="24"
