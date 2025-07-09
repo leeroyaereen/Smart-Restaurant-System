6dfe70c0c20d28d047ae53af16fa9a15825d0adb
leeroyaereen <142387082+leeroyaereen@users.noreply.github.com>
2025-07-09 23:39:43 +0545

    Added Payment Facilitation
    Added Khalti payment php page
    
    Added OTP Box
    
    Added Logical structureing in payment.js

--- public/assets/js/Payment.js
+++ public/assets/js/Payment.js
@@ -1,12 +1,44 @@
-const form = document.querySelector('#paymentForm');
-const esewaID = document.querySelector("#esewaID");
-const mpin = document.querySelector("#MPIN");
 
-document.onload = function(){
-    form.addEventListener("submit", OnSubmit(event));
-    const confirmPaymentBoxDiv = document.querySelector("#confirmPaymentBox");
-    confirmPaymentBoxDiv.style.display = "block";
-}
+let form;
+let khaltiID;
+let mpin;
+let priceAmount;
+
+let confirmPaymentBoxDiv;
+let confirmPaymentButton;
+let confirmUsername;
+let confirmKhaltiID;
+let confirmRestaurantName;
+
+let otpBox;
+let confirmOTP;
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    form = document.querySelector('#paymentForm');
+    khaltiID = document.querySelector("#khaltiID");
+    mpin = document.querySelector("#MPIN");
+    priceAmount = document.querySelector("#priceAmount");
+
+    confirmPaymentButton = document.querySelector("#confirmPayment");
+    confirmUsername = document.querySelector("c_username");
+    confirmKhaltiID = document.querySelector("c_khaltiID");
+    confirmRestaurantName = document.querySelector("c_restaurantName");
+
+    confirmOTP = document.querySelector("#confirmOTP");
+
+    form.addEventListener("submit", (event) => {
+        OnSubmit(event);
+    });
+    confirmPaymentBoxDiv = document.querySelector("#comfirmPayment-box");
+    confirmPaymentBoxDiv.style.display = "none";
+
+    otpBox = document.querySelector("#OTP-box");
+    if (otpBox) {
+        otpBox.style.display = "none";
+    }
+});
 function CancelPaymentAndReturn(){
     window.location.href = "Review";
 
@@ -16,10 +48,9 @@ function OnSubmit(event){
     event.preventDefault();
     alert("PaymentSuccess");
     let isValidForm = true;
-    isValidForm = isValidPhoneNumber(esewaID.value) && isValidMPIN(mpin.value);
+    isValidForm = isValidPhoneNumber(khaltiID.value) && isValidMPIN(mpin.value);
     if(!isValidForm){
         return;
     }   
-    window.location.href = "paymentConfirm"; 
-    window.location.href = "https:rc-epay.esewa.com.np/api/epay/main/v2/form";
+
 }
--- public/views/Payment.php
+++ public/views/Payment.php
@@ -5,9 +5,10 @@
  
         <title> Payment</title>
         <script src="assets/js/config.js.php"></script>
-        <script src="assets/js/payment.js"></script>
+        <script src="assets/js/Payment.js"></script>
         <script src="assets/js/Utilities.js"></script>
         <link rel="stylesheet" href="assets/css/Payment.css">
+        <link rel="stylesheet" href="assets/css/PageStructure.css">
     </head>
     <body>
         <div class="payment-container">
@@ -16,17 +17,17 @@
 
                 <form id="paymentForm"  method="POST">
                     <div class="input-group">
-                        <label for="esewaID"> Esewa ID </label>
-                        <input type="tel" id="esewaID" placeholder="Your Esewa Id" required>
+                        <label for="khaltiID"> Khalti ID </label>
+                        <input type="tel" id="khaltiID" placeholder="Your Khalti Id" required>
                     </div>
                     
                     <div class="input-group">
                         <label for="MPIN"> MPIN </label>
-                        <input type="password" id="MPIN" placeholder="Your Esewa Id" required>
+                        <input type="password" id="MPIN" placeholder="Your Khalti MPIN" required>
                     </div>
                     
                     <div class="input-group" >
-                        <button type="submit" class="payment-button"> Next</button>
+                        <button type="submit" class="payment-button"> Pay Rs <span id='priceAmount'></span></button>
                         <button class="back" onclick="CancelPaymentAndReturn()">Cancel</button>
                     </div>
                     
@@ -34,41 +35,43 @@
             </div>
 
             <div class="payment-box" id="comfirmPayment-box">
+                <h2>Confirm Payment</h2>
                 <table class="">
                     <tr class="tableRow">
-                        <td>Esewa ID :</td>
-                        <td id="c_esewaID"></td>
+                        <td>Khalti ID :</td>
+                        <td id="c_khaltiID">0000000</td>
                     </tr>
                     <tr class="tableRow">
                         <td>Sender Name :</td>
-                        <td id="c_esewaID"></td>
+                        <td id="c_username">abc</td>
                     </tr>
                     <tr class="tableRow">
                         <td>Reciever Name :</td>
-                        <td id="c_esewaID"></td>
+                        <td id="c_restaurantName">abc</td>
                     </tr>
                     <tr class="tableRow">
                         <td>Amount :</td>
-                        <td id="c_esewaID"></td>
+                        <td> <span id="priceAmount">00000</span></td>
                     </tr>
                 </table>
-                <button class="confirm-button">Confirm</button>
+                <button class="confirm-button" id="confirmPayment">Confirm</button>
+            </div>
+
+            <div class="payment-box" id="OTP-box">
+                <form action="pay.php" method="post">
+                    <!-- Requires value from backend -->
+                    <input type="hidden" name="token">
+                    <!-- Requires value from backend -->
+                    <input type="hidden" name="mpin">
+                    <h2>OTP </h2>
+                    <div class="input-group">                    
+                        <input type="number" value="" name="otp" placeholder="xxxx">
+                    </div>
+                    <button id="confirmOTP" class="confirm-button">Confirm OTP</button>
+                </form>
             </div>
         </div>
-        <!-- <form action="https://rc-epay.esewa.com.np/api/epay/main/v2/form" method="POST">
-    <input type="text" id="amount" name="amount" value="100" required>
-    <input type="text" id="tax_amount" name="tax_amount" value ="10" required>
-    <input type="text" id="total_amount" name="total_amount" value="110" required>
-    <input type="text" id="transaction_uuid" name="transaction_uuid" value="241028" required>
-    <input type="text" id="product_code" name="product_code" value ="EPAYTEST" required>
-    <input type="text" id="product_service_charge" name="product_service_charge" value="0" required>
-    <input type="text" id="product_delivery_charge" name="product_delivery_charge" value="0" required>
-    <input type="text" id="success_url" name="success_url" value="https://developer.esewa.com.np/success" required>
-    <input type="text" id="failure_url" name="failure_url" value="https://developer.esewa.com.np/failure" required>
-    <input type="text" id="signed_field_names" name="signed_field_names" value="total_amount,transaction_uuid,product_code" required>
-    <input type="text" id="signature" name="signature" value="i94zsd3oXF6ZsSr/kGqT4sSzYQzjj1W/waxjWyRwaME=" required>
-    <input value="Submit" type="submit">
-    </form> -->
+    
     </body>
 </html>
  
